de02d76f0c53da05353953ec5f50d28136db930a
Maria Kruleva <mariakruleva@gmail.com>
2026-05-14 17:49:05 +0400

    приложение team-finder

--- team_finder/urls.py
+++ team_finder/urls.py
@@ -1,9 +1,15 @@
-from django.contrib import admin
-from django.shortcuts import redirect
-from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-
+    path('admin/', admin.site.urls),
+    path('projects/', include('projects.urls')),
+    path('users/', include('users.urls')),
+    path('', RedirectView.as_view(url='/projects/list/', permanent=False)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
